56cd827f5ff8e92784f07a8c8d24e4796378857d
Millie Cheung <cheungmillie9@gmail.com>
2022-05-30 11:44:59 +0200

    added secret

--- server.js
+++ server.js
@@ -2,9 +2,10 @@ const express = require("express");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+const secret = process.env.SECRET || "no secret";
 
 app.get("/", (req, res) => {
-  res.send("Hello World" );
+  res.send("Hello World, this ia a secret" + secret + " but dont tell anyone");
 });
 
 app.listen(PORT, () => {
