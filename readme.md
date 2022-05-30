1c998db540922ac7155fb5c108bfea306ec9d8cd
Millie Cheung <cheungmillie9@gmail.com>
2022-05-30 09:29:26 +0200

    added readme

---
+++ readme.md
@@ -0,0 +1,5 @@
+# Welcome to Monday Mornings
+
+## Install
+
+Clone and run `npm i & npm start`
--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const express = require("express ");
+const express = require("express");
 
 const app = express();
 
@@ -6,6 +6,6 @@ app.get("/", (req, res) => {
   res.send("Hello World");
 });
 
-app.list(3000, () => {
+app.listen(3000, () => {
   console.log("Server is running on port 3000.");
 });
